e0deb61e9e2dfabe6f3d63ecda92533a88545779
ViolOdn <veta0205@mail.ru>
2019-09-11 16:25:24 +0300

    editing book info was added

--- library/views.py
+++ library/views.py
@@ -18,9 +18,21 @@ def add_user(request):
 def generate_list_of_books(request, query):
     current_reader = Reader.objects.filter(username=query).first()
     books_list = Book.objects.filter(user_id=current_reader)
-    s = books_list
-    final_list = []
-    for book in books_list:
-        final_list.append(book.book_name)
-    return render(request, 'list_of_books.html', {'books': final_list})
+    return render(request, 'list_of_books.html', {'books': books_list, 'reader': current_reader})
+
+
+def open_edit_book_form(request, query):
+    current_book = Book.objects.filter(id=query).first()
+    return render(request, 'edit_book.html', {'book': current_book})
+
+
+@csrf_exempt
+def editing_book(request):
+    new_book_name = request.POST['book_name']
+    new_book_author = request.POST['author']
+    new_book_year = request.POST['year']
+    Book.objects.filter(id=request.POST['id']).update(book_name=new_book_name, author=new_book_author, year=new_book_year)
+    return HttpResponseRedirect('/')
+
+
 
--- library_project_new/urls.py
+++ library_project_new/urls.py
@@ -5,5 +5,7 @@ from django.contrib import admin
 urlpatterns = [
     path('', main_page),
     path('successful_register', add_user),
-    path('list_of_books/<query>', generate_list_of_books, name='generate_list_of_books')
+    path('list_of_books/<query>', generate_list_of_books, name='generate_list_of_books'),
+    path('edit_book/<query>', open_edit_book_form, name='open_edit_book_form'),
+    path('successful_editing', editing_book)
 ]
